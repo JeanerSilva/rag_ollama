d29095061928625933fbf2a5ae8efe8b1f7aaf1d
JeanerSilva <jeanerufmg@gmail.com>
2025-05-21 20:00:57 -0300

    ult

--- rag/llm_loader.py
+++ rag/llm_loader.py
@@ -15,6 +15,7 @@ def load_llm(modelo_llm: str):
     if modelo_llm == "GGUF (offline)":
         return LlamaCpp(
             model_path="./.models/Meta-Llama-3-8B-Instruct.Q5_K_M.gguf",
+
             n_ctx=4096,
             n_batch=64,
             temperature=TEMPERATURE,
